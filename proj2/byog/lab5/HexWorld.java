aa3efd406689d0b796070467adb38a7868c298f9
wkKarry <1415546982@qq.com>
2024-09-23 15:57:52 +0800

    /*beta*/ lab05,绘制六边形方法完成.

--- proj2/byog/lab5/HexWorld.java
+++ proj2/byog/lab5/HexWorld.java
@@ -1,5 +1,7 @@
 package byog.lab5;
+
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 import byog.TileEngine.TERenderer;
@@ -12,4 +14,18 @@ import java.util.Random;
  * Draws a world consisting of hexagonal regions.
  */
 public class HexWorld {
+
+    public static void main(String[] args) {
+        TERenderer ter = new TERenderer();
+        ter.initialize(20, 20);
+        TETile[][] world = new TETile[20][20];
+        Position p = new Position(0, 0);
+        for (int i = 0; i < 20; i++) {
+            for (int j = 0; j < 20; j++) {
+                world[i][j] = Tileset.WALL;
+            }
+        }
+        addHexagon.addhexagon(p, 4, Tileset.FLOWER, world);
+        ter.renderFrame(world);
+    }
 }
