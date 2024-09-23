870936227c23e0cc8e40a7c4b588273bd70c29c2
wkKarry <1415546982@qq.com>
2024-09-23 19:14:36 +0800

    lab05完成,能随机"铺六边形"

--- proj2/byog/lab5/HexWorld.java
+++ proj2/byog/lab5/HexWorld.java
@@ -17,15 +17,43 @@ public class HexWorld {
 
     public static void main(String[] args) {
         TERenderer ter = new TERenderer();
-        ter.initialize(20, 20);
-        TETile[][] world = new TETile[20][20];
-        Position p = new Position(0, 0);
-        for (int i = 0; i < 20; i++) {
-            for (int j = 0; j < 20; j++) {
-                world[i][j] = Tileset.WALL;
+        ter.initialize(100, 100);
+        TETile[][] world = new TETile[100][100];
+        Position p = new Position(50, 50);
+        for (int i = 0; i < 100; i++) {
+            for (int j = 0; j < 100; j++) {
+                world[i][j] = Tileset.NOTHING;
             }
         }
-        addHexagon.addhexagon(p, 4, Tileset.FLOWER, world);
+        addHexagon.addhexagon(p, 3, Tileset.FLOWER, world);
+        for (int i = 0; i < 10; i++) {
+            p = nextPosition(p, 3);
+            if (i % 3 == 0) {
+                addHexagon.addhexagon(p, 3, Tileset.FLOOR, world);
+            } else if (i % 3 == 1) {
+                addHexagon.addhexagon(p, 3, Tileset.FLOWER, world);
+            } else {
+                addHexagon.addhexagon(p, 3, Tileset.WALL, world);
+            }
+        }
         ter.renderFrame(world);
     }
+
+    private static Position nextPosition(Position p, int size) {
+        double rand = Math.random();
+        Position nextP;
+        if (rand <= 0.25 && p.x + 2 * size - 2 > 0 && p.y + 2 * size - 2 > 0) {
+            nextP = new Position(p.x + 2 * size - 2, p.y + 2 * size - 2);
+        } else if (rand <= 0.5 && p.x - 2 * size + 2 > 0 && p.y + 2 * size - 2 > 0) {
+            nextP = new Position(p.x - 2 * size + 2, p.y + 2 * size - 2);
+        } else if (rand <= 0.75 && p.x - 2 * size + 2 > 0 && p.y - 2 * size + 2 > 0) {
+            nextP = new Position(p.x - 2 * size + 2, p.y - 2 * size + 2);
+        } else if (p.x + 2 * size - 2 > 0 && p.y - 2 * size + 2 > 0) {
+            nextP = new Position(p.x + 2 * size - 2, p.y - 2 * size + 2);
+        }else{
+            nextP = p;
+        }
+        return nextP;
+    }
+
 }
