0b9b468d6d184b1aacff61438b619557fb27574a
wkKarry <1415546982@qq.com>
2024-10-15 20:55:01 +0800

    /*beta*/ 基于二维数组的实现.没用到Disjoint Set.

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -1,7 +1,59 @@
 package hw2;
 
+import edu.princeton.cs.algs4.NFA;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    boolean[][] grid;
+    int max_index = 0;
+
+    public Percolation(int N) {
+        boolean[][] grid = new boolean[N][N];
+        max_index = N-1;
+    }             // create N-by-N grid, with all sites initially blocked
+
+    public void open(int row, int col) {
+        grid[row][col] = true;
+    }       // open the site (row, col) if it is not open already
+
+    public boolean isOpen(int row, int col) {
+        return grid[row][col];
+    }  // is the site (row, col) open?
+
+    public boolean isFull(int row, int col) {
+        if(col<0||col>max_index||isOpen(row, col)) {
+            return false;
+        }
+        if (row == 0 && isOpen(row, col)) {
+            return true;
+        } else {
+            return isFull(row - 1, col) || isFull(row, col + 1) || isFull(row, col - 1);
+        }
+    }  // is the site (row, col) full?
+
+    public int numberOfOpenSites() {
+        int count = 0;
+        for (int i = 0; i < max_index; i++) {
+            for (int j = 0; j < max_index; j++) {
+                if (grid[i][j]) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }           // number of open sites
+
+    public boolean percolates() {
+        for (int i = 0; i <= max_index; i++) {
+            if (isFull(max_index,i)) {
+                return true;
+            }
+        }
+        return false;
+    }              // does the system percolate?
+
+    public static void main(String[] args) {
+    }   // use for unit testing (not required)
+
 
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,38 @@
 package hw2;
 
-public class PercolationStats {
+import edu.princeton.cs.algs4.*;
 
+public class PercolationStats {
+    private double[] percolationProbabilities;
+    private int trials;
+
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        if (N <= 0 || T <= 0) throw new IllegalArgumentException();
+        percolationProbabilities = new double[T];
+        for (int k = 0; k < T; k++) {
+            Percolation matrix = pf.make(N);
+            while (!matrix.percolates()) {
+                int i = StdRandom.uniform(N);
+                int j = StdRandom.uniform(N);
+                matrix.open(i, j);
+            }
+            percolationProbabilities[k]+= matrix.numberOfOpenSites()/(N*N);
+        }
+    }   // perform T independent experiments on an N-by-N grid
+
+    public double mean(){
+        return StdStats.mean(percolationProbabilities);
+    }                                           // sample mean of percolation threshold
+
+    public double stddev(){
+        return StdStats.stddev(percolationProbabilities);
+    }                                         // sample standard deviation of percolation threshold
+
+    public double confidenceLow(){
+        return mean()-(1.96*stddev())/Math.sqrt(trials);
+    }                                  // low endpoint of 95% confidence interval
+
+    public double confidenceHigh(){
+        return mean()+(1.96*stddev())/Math.sqrt(trials);
+    }                                 // high endpoint of 95% confidence interval
 }
