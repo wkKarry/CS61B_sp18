b6c4c05b9db05ae0667d0c23a0c5c95ab1600e6f
wkKarry <1415546982@qq.com>
2024-10-18 13:18:46 +0800

    答案

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -1,83 +1,120 @@
 package hw2;
 
-import edu.princeton.cs.algs4.NFA;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
-    public int range;
-    public int num;
-    private class Site{
-        public int no;
-        public boolean open;
-        public Site(int i, int j){
-            no = i*range +j + 1;
-            open = false;
+    private class Site {
+        boolean open;
+        boolean isConnectedBottom;
+
+        Site() {
+            this.open = false;
+            this.isConnectedBottom = false;
         }
-    }
-    private Site[][] map;
-    private WeightedQuickUnionUF uf;
-    public Percolation(int n) {
-        if(n<=0) throw new IllegalArgumentException();
-        range = n;
-        map = new Site[n][n];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                map[i][j] = new Site(i, j);
-            }
-        }
-        uf = new WeightedQuickUnionUF(range * range + 2);
     }
 
-    private boolean inValid(int index){
-        return (index<0 ||index >=range);
+    private Site[][] sites;
+    private int numberOfOpenSites;
+    private WeightedQuickUnionUF uf;
+    private Site topVirtualSite;
+    private int topIndex;
+    private boolean percolated;
+
+    public Percolation(int N) {
+        if (N <= 0) {
+            throw new IllegalArgumentException("N < 0");
+        }
+        sites = new Site[N][N];
+        for (Site[] row : sites) {
+            for (int i = 0; i < N; i++) {
+                row[i] = new Site();
+            }
+        }
+
+        numberOfOpenSites = 0;
+        uf = new WeightedQuickUnionUF(N * N + 1);
+        topIndex = uf.count() - 1;
+        topVirtualSite = new Site();
+        percolated = false;
+        for (int i = 0; i < sites[0].length; i++) {
+            uf.union(i, topIndex);
+            sites[sites.length - 1][i].isConnectedBottom = true;
+        }
+    }
+
+    private int xyTo1D(int r, int c) {
+        return r * sites.length + c;
+    }
+
+    private Site indexToSite(int index) {
+        if (index != topIndex) {
+            int r = index / sites.length;
+            int c = index - r * sites.length;
+            return sites[r][c];
+        } else {
+            return topVirtualSite;
+        }
+    }
+
+    private boolean invalid(int row, int col) {
+        return row < 0 || row >= sites.length || col < 0 || col >= sites.length;
+    }
+
+    private void addUnion(int row, int col, int dr, int dc) {
+        if (!invalid(row + dr, col + dc) && isOpen(row + dr, col + dc)) {
+            int p1 = uf.find(xyTo1D(row, col));
+            int p2 = uf.find(xyTo1D(row + dr, col + dc));
+            uf.union(xyTo1D(row, col), xyTo1D(row + dr, col + dc));
+            int newParent = uf.find(xyTo1D(row, col));
+            indexToSite(newParent).isConnectedBottom =
+                    indexToSite(p1).isConnectedBottom || indexToSite(p2).isConnectedBottom;
+        }
     }
 
 
     public void open(int row, int col) {
-        if(inValid(row) || inValid(col)) throw new IndexOutOfBoundsException();
-        if(map[row][col].open) return;
-        map[row][col].open = true;
-        if (row == 0) {
-            uf.union(map[row][col].no,0);
+        if (invalid(row, col)) {
+            throw new IndexOutOfBoundsException("Open " + row + "," + col);
         }
-        if (row-1>=0 && map[row-1][col].open) {
-            uf.union(map[row-1][col].no,map[row][col].no);
+        if (!sites[row][col].open) {
+            sites[row][col].open = true;
+            addUnion(row, col, 0, 1);
+            addUnion(row, col, 0, -1);
+            addUnion(row, col, 1, 0);
+            addUnion(row, col, -1, 0);
+            numberOfOpenSites += 1;
+
+            if (!percolated) {
+                int parent = uf.find(xyTo1D(row, col));
+                if (indexToSite(parent).isConnectedBottom && uf.connected(parent, topIndex)) {
+                    percolated = true;
+                }
+            }
         }
-        if (row+1<range && map[row+1][col].open) {
-            uf.union(map[row+1][col].no,map[row][col].no);
-        }
-        if (col-1>=0 && map[row][col-1].open) {
-            uf.union(map[row][col-1].no,map[row][col].no);
-        }
-        if (col+1<range && map[row][col+1].open) {
-            uf.union(map[row][col+1].no,map[row][col].no);
-        }
-        /*if (row == range - 1){
-            uf.union(map[row][col].no,range * range + 1);
-        }*/
-        num++;
     }
 
     public boolean isOpen(int row, int col) {
-        if(inValid(row) || inValid(col)) throw new IndexOutOfBoundsException();
-        return map[row][col].open;
-    }
-    public boolean isFull(int row, int col) {
-        if(inValid(row) || inValid(col)) throw new IndexOutOfBoundsException();
-        return uf.connected(map[row][col].no,0);
-    }
-    public int numberOfOpenSites(){
-        return num;
-    }
-    /*public boolean percolates(){
-        return uf.connected(0,range * range + 1);
-    }*/
-    public boolean percolates() {
-        for (int i = range*(range-1)+1; i < range*range + 1; i++) {
-            if (uf.connected(i,0)){
-                return true;
-            }
+        if (invalid(row, col)) {
+            throw new IndexOutOfBoundsException("isOpen " + row + "," + col);
         }
-        return false;
+        return sites[row][col].open;
+    }
+
+    public boolean isFull(int row, int col) {
+        if (invalid(row, col)) {
+            throw new IndexOutOfBoundsException("isFull " + row + "," + col);
+        }
+        return isOpen(row, col) && uf.connected(xyTo1D(row, col), topIndex);
+    }
+
+    public int numberOfOpenSites() {
+        return numberOfOpenSites;
+    }
+
+    public boolean percolates() {
+        return percolated;
+    }
+
+    public static void main(String[] args) {
     }
 }
